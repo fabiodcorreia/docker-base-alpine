c06548be2de5d2af126ce5c5c2e3a0bd5312f406
Fábio Correia <fabiodcorreia@gmail.com>
2020-07-07 18:41:47 +0100

    upgrade packages and fix warning on usermod script

--- packages.sh
+++ packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build . -t base
-docker run --rm --entrypoint '/bin/sh' -v ${PWD}:/tmp base -c '\
+IMAGE_NAME="base-alpine"
+
+docker build . -t ${IMAGE_NAME}
+docker run --rm --entrypoint '/bin/sh' -v ${PWD}:/tmp ${IMAGE_NAME} -c '\
   apk info -v | sort > /tmp/package_versions.txt && \
   chmod 777 /tmp/package_versions.txt'
